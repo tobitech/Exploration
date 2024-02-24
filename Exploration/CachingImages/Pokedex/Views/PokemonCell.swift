4186f60bd0eb8aa69898a15850415de95327fa7d
Tobi Omotayo <tobitech@ymail.com>
2024-02-24 14:32:47 +0100

    handle phases in async image

--- Exploration/CachingImages/Pokedex/Views/PokemonCell.swift
+++ Exploration/CachingImages/Pokedex/Views/PokemonCell.swift
@@ -1,19 +1,37 @@
 import SwiftUI
 
 struct PokemonCell: View {
-	var pokemon: Pokemon
+	let pokemon: Pokemon
+	private let imageWidth = 110.0
+	private let cellHeight = 130.0
 	
 	var body: some View {
 		HStack {
-			AsyncImage(url: pokemon.url)
-			{ image in
-				image.resizable()
-			} placeholder: {
-				Color.secondary.opacity(0.3)
+			AsyncImage(url: pokemon.url) { phase in
+				switch phase {
+				case let .success(image):
+					HStack {
+						image
+							.resizable()
+							.aspectRatio(contentMode: .fit)
+							.frame(width: imageWidth)
+							.padding(.trailing, 10)
+						PokemonDescriptionView(pokemon: pokemon)
+						Spacer()
+					}
+				case let .failure(error):
+					ErrorView(error: error)
+				case .empty:
+					HStack {
+						ProgressView()
+							.progressViewStyle(.circular)
+							.tint(.red)
+						Spacer()
+					}
+				@unknown default:
+					Image(systemName: "questionmark")
+				}
 			}
-			.frame(width: 120, height: 120)
-			PokemonDescriptionView(pokemon: pokemon)
-			Spacer()
 		}
 	}
 }
