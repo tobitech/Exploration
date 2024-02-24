07cb4d3028dc555e28ae082418273f485be8dc4f
Tobi Omotayo <tobitech@ymail.com>
2024-02-24 11:01:36 +0100

    Initial Commit

---
+++ Exploration/ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorationApp.swift
+//  Exploration
+//
+//  Created by Oluwatobi Omotayo on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
