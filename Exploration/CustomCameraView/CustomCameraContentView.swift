77dc615a135c5187e06490ae23caa04470e9a1f0
Tobi Omotayo <tobitech@ymail.com>
2024-05-20 23:20:03 +0100

    setup and implement a custom camera view.

---
+++ Exploration/CustomCameraView/CustomCameraContentView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCameraContentView.swift
+//  Exploration
+//
+//  Created by Oluwatobi Omotayo on 20/05/2024.
+//
+
+import SwiftUI
+
+struct CustomCameraContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCameraContentView()
+}
---
+++ Exploration/CustomCameraView/CustomCameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCameraView.swift
+//  Exploration
+//
+//  Created by Oluwatobi Omotayo on 20/05/2024.
+//
+
+import SwiftUI
+
+struct CustomCameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCameraView()
+}
