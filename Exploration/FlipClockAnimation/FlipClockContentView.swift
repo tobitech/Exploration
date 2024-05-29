ae807e610141466909b63b4523fb6d45d0a3136b
Tobi Omotayo <tobitech@ymail.com>
2024-05-29 17:37:59 +0100

    add a 60sec countdown timer using the flip clock implementation.

--- Exploration/FlipClockAnimation/FlipClockContentView.swift
+++ Exploration/FlipClockAnimation/FlipClockContentView.swift
@@ -8,29 +8,49 @@
 import SwiftUI
 
 struct FlipClockContentView: View {
+	@State private var timer: CGFloat = 0
 	@State private var count: Int = 0
 	
 	var body: some View {
 		NavigationStack {
 			VStack {
-				FlipClockTextEffect(
-					value: $count, 
-					size: CGSize(
-						width: 100,
-						height: 150
-					),
-					fontSize: 70,
-					cornerRadius: 10,
-					foreground: .white,
-					background: .red
-				)
-				
-				Button("Update") {
-					count += 1
+				HStack {
+					FlipClockTextEffect(
+						value: .constant(count / 10),
+						size: CGSize(
+							width: 100,
+							height: 150
+						),
+						fontSize: 70,
+						cornerRadius: 10,
+						foreground: .white,
+						background: .red
+					)
+					
+					FlipClockTextEffect(
+						value: .constant(count % 10),
+						size: CGSize(
+							width: 100,
+							height: 150
+						),
+						fontSize: 70,
+						cornerRadius: 10,
+						foreground: .white,
+						background: .red
+					)
 				}
-				.buttonStyle(.borderedProminent)
-				.tint(.blue)
-				.padding(.top, 45)
+				.onReceive(Timer.publish(every: 0.01, on: .current, in: .common).autoconnect(), perform: { _ in
+					timer += 0.01
+					if timer >= 60 { timer = 0 }
+					count = Int(timer)
+				})
+				
+//				Button("Update") {
+//					count += 1
+//				}
+//				.buttonStyle(.borderedProminent)
+//				.tint(.blue)
+//				.padding(.top, 45)
 			}
 			.padding()
 		}
