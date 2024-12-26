08811207f7052ea9afa1d7209c0b8c22b0522161
Tobi Omotayo <tobitech@ymail.com>
2024-12-26 03:35:28 +0100

    add a new version for stack cards.

--- Exploration/ScrollTransition/ScrollTransitionContentView.swift
+++ Exploration/ScrollTransition/ScrollTransitionContentView.swift
@@ -15,30 +15,48 @@ struct ScrollTransitionContentView: View {
 			ScrollView(.horizontal) {
 				LazyHStack(spacing: 12) {
 					ForEach(images) { image in
-						Image(image.image)
-							.resizable()
-							.aspectRatio(contentMode: .fill)
-						/* Type 2: Parallax
-							/// Extend the image with how much shifting you've applied in the scrollTransition modifier. In my case, I shifted the view about 80, and thus I used the value `80` here.
-							.frame(width: 220 + 80)
-							.scrollTransition(.interactive, axis: .horizontal) { content, phase in
-								content
-									.offset(x: phase == .identity ? 0 : -phase.value * 80)
-							}
-						*/
-							.frame(width: 220, height: size.height)
-							.clipShape(.rect(cornerRadius: 25))
-						/* Type 1: Circular Carousel
-							.scrollTransition(.interactive, axis: .horizontal) { content, phase in
-								content
-									/// Let's make the inactive cards blurry.
-									.blur(radius: phase == .identity ? 0 : 2, opaque: false)
-									.scaleEffect(phase == .identity ? 1 : 0.9, anchor: .bottom)
-									/// Make it a circular carousel
-									.offset(y: phase == .identity ? 0 : 35)
-									.rotationEffect(.init(degrees: phase == .identity ? 0 : phase.value * 15), anchor: .bottom)
-							}
-						*/
+						/// Since Stack adds items on top of one another, that's why the last item is displaying first. This can be simply resolved with the help of the zindex modifier.
+						let index = Double(images.firstIndex(where: { $0.id == image.id }) ?? 0)
+						
+						GeometryReader { // Added as part of Type 3 code.
+							let minX = $0.frame(in: .scrollView(axis: .horizontal)).minX
+							
+							Image(image.image)
+								.resizable()
+								.aspectRatio(contentMode: .fill)
+							/* Type 2: Parallax
+							 /// Extend the image with how much shifting you've applied in the scrollTransition modifier. In my case, I shifted the view about 80, and thus I used the value `80` here.
+							 .frame(width: 220 + 80)
+							 .scrollTransition(.interactive, axis: .horizontal) { content, phase in
+							 content
+							 .offset(x: phase == .identity ? 0 : -phase.value * 80)
+							 }
+							 */
+								.frame(width: 220, height: size.height)
+								.clipShape(.rect(cornerRadius: 25))
+							/* Type 1: Circular Carousel
+							 .scrollTransition(.interactive, axis: .horizontal) { content, phase in
+							 content
+							 /// Let's make the inactive cards blurry.
+							 .blur(radius: phase == .identity ? 0 : 2, opaque: false)
+							 .scaleEffect(phase == .identity ? 1 : 0.9, anchor: .bottom)
+							 /// Make it a circular carousel
+							 .offset(y: phase == .identity ? 0 : 35)
+							 .rotationEffect(.init(degrees: phase == .identity ? 0 : phase.value * 15), anchor: .bottom)
+							 }
+							 */
+							/* Type 3: Stacked Cards */
+								.scrollTransition(.interactive, axis: .horizontal) { content, phase in
+									content
+										.blur(radius: phase == .identity ? 0 : 2, opaque: false)
+										.scaleEffect(phase == .identity ? 1 : 0.9, anchor: .bottom)
+										.offset(y: phase == .identity ? 0 : -10)
+										.rotationEffect(.init(degrees: phase == .identity ? 0 : phase.value * 5), anchor: .bottomTrailing)
+										.offset(x: minX < 0 ? minX / 2 : -minX)
+								}
+						}
+						.frame(width: 220)
+						.zIndex(-index)
 					}
 				}
 				.scrollTargetLayout()
